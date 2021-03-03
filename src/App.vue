396212e1233216fa1aa996794ce0a553af2a3005
litong <1359088073@qq.com>
2021-03-03 11:48:09 +0800

    测试自动部署

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
       <p>java0821 全体同学 前程似锦</p>
       <p>java0821 全体同学 越来越帅</p>
-      <p>祝 java0821 全体同学 拿到自己心仪的offer</p>
+      <p>祝 java0821 全体同学 面试顺利</p>
       <p>祝 java0821 全体同学 月薪20k</p>
    <img src="http://img12.360buyimg.com/n1/s450x450_jfs/t1/142295/6/19866/63874/5fe4408eE401889c8/8f5c2b37676a2264.jpg"/>
   </div>
